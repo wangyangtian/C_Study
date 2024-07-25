a77eb8749e4251b86ab2151cf087d1c2df8fd747
Dylan <15155562278@qq.com>
2024-07-25 15:36:10 +0800

    顺序栈和链栈的实现

---
+++ Data structure/Stack and queue/Linked_stack.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Linked_stack
+{
+    int data;
+    struct Linked_stack *next;
+}*LinkStack;
+
+
+int main() {
+    
+    return 0;
+}
---
+++ Data structure/Stack and queue/Sequential_stack.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// 顺序栈
+
+#define MAXSIZE 10 // 定义栈中元素的最大个数
+
+typedef struct Sequential_stack
+{
+    int data[MAXSIZE]; // 静态数组字放栈中元素
+    int top;           // 栈顶指针
+} SqStack;
+
+void InitStack(SqStack *S)
+{
+    (*S).top = -1; // 初始化栈顶指针
+}
+
+bool EmptyStack(SqStack S)
+{
+    if (S.top == -1)
+    {
+        printf("栈空！");
+        return true;
+    }
+    else
+    {
+        printf("非空栈！");
+        return false;
+    }
+}
+
+// 压栈操作
+bool Push(SqStack *S, int elem)
+{
+    if (S->top == MAXSIZE - 1)
+    {                 // 判断栈是否已满
+        return false; // 栈满，无法压栈
+    }
+    S->data[++(S->top)] = elem; // 栈顶指针先加1，然后将元素压入栈顶
+    return true;                // 压栈成功
+}
+
+// 出栈操作
+bool Pop(SqStack *S, int *elem)
+{
+    if (S->top == -1)
+    {                 // 判断栈是否为空
+        return false; // 栈空，无法出栈
+    }
+    *elem = S->data[(S->top)--]; // 将栈顶元素赋值给elem，栈顶指针减1
+    return true;                 // 出栈成功
+}
+
+bool GetTop(SqStack *S, int *top)
+{
+    if (S->top == -1)
+        return false;
+    *top = S->data[S->top];
+    return true;
+}
+
+int main()
+{
+    SqStack S;
+    InitStack(&S); // 初始化栈
+
+    return 0;
+}
