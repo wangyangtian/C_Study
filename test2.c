bc24d2fa5212fd75106d52c08aef347edf7fc8df
Dylan <15155562278@qq.com>
2024-08-15 11:29:49 +0800

    8.15--数组相关练习题

--- test2.c
+++ test2.c
@@ -1,61 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct {
-    char *ch;
-    int length;
-} HString;
-
-HString *SubCat(HString str1, HString str2, int i, int j) {
-    if (i > str1.length || i > j) return NULL;
-    
-    HString *ret = (HString *)malloc(sizeof(HString));
-    ret->length = str1.length + str2.length - (j - i + 1);
-    ret->ch = (char *)malloc((ret->length + 1) * sizeof(char)); // +1 for null-terminator
-    
-    int n1 = 0, n2 = 0;
-    
-    // Copy str1 up to i
-    for (int k = 0; k < i; ++k) {
-        ret->ch[n1++] = str1.ch[k];
+void findSpecialElements(int A[][100], int m, int n) {
+    // 遍历矩阵中的每个元素
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            int current = A[i][j];
+            
+            // 查找所在行的最小值和最大值
+            int rowMin = A[i][0], rowMax = A[i][0];
+            for (int k = 1; k < n; k++) {
+                if (A[i][k] < rowMin) rowMin = A[i][k];
+                if (A[i][k] > rowMax) rowMax = A[i][k];
+            }
+            
+            // 查找所在列的最小值和最大值
+            int colMin = A[0][j], colMax = A[0][j];
+            for (int k = 1; k < m; k++) {
+                if (A[k][j] < colMin) colMin = A[k][j];
+                if (A[k][j] > colMax) colMax = A[k][j];
+            }
+            
+            // 判断是否满足条件1：既是所在行中的最小值，又是所在列中的最小值
+            if (current == rowMin && current == colMin) {
+                printf("元素(%d, %d) = %d 是行和列的最小值\n", i, j, current);
+            }
+            
+            // 判断是否满足条件2：既是所在行中的最大值，又是所在列中的最大值
+            if (current == rowMax && current == colMax) {
+                printf("元素(%d, %d) = %d 是行和列的最大值\n", i, j, current);
+            }
+        }
     }
-
-    // Copy str2 entirely
-    while (str2.ch[n2]) {
-        ret->ch[n1++] = str2.ch[n2++];
-    }
-
-    // Copy str1 from j onwards
-    for (int k = j + 1; k < str1.length; ++k) {
-        ret->ch[n1++] = str1.ch[k];
-    }
-    
-    ret->ch[n1] = '\0'; // null-terminate the string
-    return ret;
 }
 
 int main() {
-    HString str1, str2;
+    int A[100][100] = {
+        {10, 2, 5},
+        {3, 7, 9},
+        {8, 6, 4}
+    };
+    int m = 3, n = 3;
     
-    str1.ch = "Hello, World!";
-    str1.length = strlen(str1.ch);
-    
-    str2.ch = "INSERT";
-    str2.length = strlen(str2.ch);
-    
-    int i = 7;  // Starting index to replace (0-based)
-    int j = 11; // Ending index to replace (inclusive)
-    
-    HString *result = SubCat(str1, str2, i, j);
-    
-    if (result) {
-        printf("Result: %s\n", result->ch);
-        free(result->ch); // Free the allocated memory for string
-        free(result);     // Free the allocated memory for HString
-    } else {
-        printf("Invalid input!\n");
-    }
+    findSpecialElements(A, m, n);
     
     return 0;
 }
